15be871de07b7ec206b05b263d11f546f8f22785
John McIver <john.mciver.iii@gmail.com>
2024-06-24 14:16:14 -0600

    [util] Container build script sets UID and GID arguments
    The UID and GID are set by querying the current environment using `id`.

--- container/build.bash
+++ container/build.bash
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-docker build --tag=pts-test:1 .
+declare -r USER_UID=$(id -u)
+declare -r USER_GID=$(id -g)
+
+docker \
+    build \
+    --build-arg="USER_UID=${USER_UID}" \
+    --build-arg="USER_GID=${USER_GID}" \
+    --tag=pts-test:1 .
