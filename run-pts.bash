7e7473aae8631cd199d22294ccce5c0652df36bb
John McIver <john.mciver.iii@gmail.com>
2024-06-25 10:26:32 -0600

    [util] Add creation of build/install directory in run-pts.bash

--- run-pts.bash
+++ run-pts.bash
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 
+declare -r PTS_INSTALL="/tmp/pts-install"
+
+if [ ! -d "$PTS_INSTALL" ]; then
+    printf "INFO: making pts install/build directory %s\n" "$PTS_INSTALL"
+    mkdir -p "$PTS_INSTALL" || exit 1
+fi
+
 docker \
     run \
     -it \
     --rm \
     --cap-add SYS_NICE \
     --mount type=bind,source="$(pwd)",target="/pts/phoronix" \
-    --mount type=bind,source="/tmp/pts-install",target="/pts/pts-install" \
+    --mount type=bind,source="$PTS_INSTALL",target="/pts/pts-install" \
     pts-test:1
