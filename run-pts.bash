019f474789415296c7fff9992480b168c11eee9e
John McIver <john.mciver.iii@gmail.com>
2024-06-27 14:33:34 -0600

    [util] Add llvm project path specification to run-pts.bash
    The --llvm=PATH argument is required by run-pts.bash. Entry point arguments to
    the container are pass via:
    
    ./run-pts.bash --llvm=PATH -- ENTRY_POINT_ARGS...

--- run-pts.bash
+++ run-pts.bash
@@ -1,10 +1,55 @@
 #!/usr/bin/bash
 
+set -u
+
+declare -r SCRIPT_NAME=${0##*/}
+declare -r SCRIPT_PATH=${0%/*}
+
+declare -r OPT_STRING="-h"
+
 declare -r PTS_INSTALL="/tmp/pts-install"
+
+declare LLVM_PATH=""
 
 if [ ! -d "$PTS_INSTALL" ]; then
     printf "INFO: making pts install/build directory %s\n" "$PTS_INSTALL"
     mkdir -p "$PTS_INSTALL" || exit 1
+fi
+
+RESULT=$(getopt \
+             --name "$SCRIPT_NAME" \
+             --options "$OPT_STRING" \
+             --longoptions "help,llvm:" \
+             -- "$@")
+
+eval set -- "$RESULT"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h | --help)
+            printf "%s\n" "usage: $SCRIPT_NAME [-h|--help] --llvm=PATH -- ENTRY_POINT_OPTIONS"
+            exit 0
+            ;;
+        --llvm)
+            shift
+            LLVM_PATH=$1
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$LLVM_PATH" ]; then
+    printf "ERROR: --llvm=PATH must be specified\n"
+    exit 1
+fi
+
+if [ ! -d "$LLVM_PATH" ]; then
+    printf "ERROR: --llvm=%s does not specify a real directory\n" "$LLVM_PATH"
+    exit 1
 fi
 
 docker \
@@ -14,4 +59,5 @@ docker \
     --cap-add SYS_NICE \
     --mount type=bind,source="$(pwd)",target="/pts/phoronix" \
     --mount type=bind,source="$PTS_INSTALL",target="/pts/pts-install" \
-    pts-test:1
+    --mount type=bind,source="$LLVM_PATH",target="/llvm" \
+    pts-test:1 $@
