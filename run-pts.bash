5921115c4609ff3977f5df7eb180b50cc853260a
John McIver <john.mciver.iii@gmail.com>
2024-07-01 17:42:09 -0600

    [util] Add CPU configuration checks to run-pts.bash

--- run-pts.bash
+++ run-pts.bash
@@ -11,14 +11,39 @@ declare -r PTS_INSTALL="/tmp/pts-install"
 
 declare LLVM_PATH=""
 
+function checkCpuSettings() {
+    declare -r cpuTurbo="/sys/devices/system/cpu/intel_pstate/no_turbo"
+    grep -q '0' $cpuTurbo
+    if [ $? -eq 0 ]; then
+        printf "INFO: Turbo boost is enabled\n"
+    fi
+    declare -r hyperThread="/sys/devices/system/cpu/smt/control"
+    grep -q 'on' $hyperThread
+    if [ $? -eq 0 ]; then
+        printf "INFO: Hyperthreading is enabled\n"
+    fi
+
+    if ! command -v cpupower &> /dev/null; then
+        printf "WARNING: cpupower command is not available\n"
+        return
+    fi
+    declare -r governor=$(cpupower frequency-info -p | sed -E -e '3!d' -e 's/\s.*+"(.*)".*/\1/')
+    if [ "$governor" = "performance" ]; then
+        printf "INFO: Performance governor is %s\n" "$governor"
+    fi
+}
+
 function helpMessage () {
     cat <<-EOF
 
 ENTRY_POINT_OPTIONS are:
 
+Build & Test:
 [-b|--build]    to build the llvm project
-[-p|--phoronix] to run Phoronix tests
 [-t|--test]     to run llvm release2 check-all target
+
+Phoronix:
+[-p|--phoronix] to run Phoronix tests
 
 EOF
 }
@@ -54,6 +79,8 @@ while [ $# -gt 0 ]; do
     esac
     shift
 done
+
+checkCpuSettings
 
 if [ -z "$LLVM_PATH" ]; then
     printf "ERROR: --llvm=PATH must be specified\n"
