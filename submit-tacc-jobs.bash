91961c8835bceb3d7b6ffb4cfbba04ee91d03e75
John McIver <john.mciver.iii@gmail.com>
2025-01-03 13:23:33 -0600

    [util] Update submit-tacc-jobs.bash with reruns

--- submit-tacc-jobs.bash
+++ submit-tacc-jobs.bash
@@ -5,7 +5,8 @@ set -u
 declare -r buildType="main"
 
 # pushd tacc-scripts
-for idNumber in {0,2,3,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21}; do
+# for idNumber in {0,2,3,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21}; do
+for idNumber in {1,2,3,5,7,8,11,13,18,21}; do
     declare scriptName=$(printf 'pts-alive2-%s-%02d.sbatch' "${buildType}" "${idNumber}")
     printf "INFO: submit %s\n" "$scriptName"
     sbatch "$scriptName"
