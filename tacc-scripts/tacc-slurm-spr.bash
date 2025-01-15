267c8d4ed2e750c7ca2172aaf5392151b6689614
John McIver <john.mciver.iii@gmail.com>
2025-01-15 16:37:58 -0700

    [util] Add TACC Sapphire Rapids Slurm script

---
+++ tacc-scripts/tacc-slurm-spr.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+#
+#SBATCH -J pts-alive2-run
+#SBATCH -p spr
+#SBATCH -N 1                    # Total nodes per job
+#SBATCH -t 48:00:00
+#SBATCH -o pts-alive2-%A_%a.out # Standard output
+#SBATCH -e pts-alive2-%A_%a.err # Standard error
+#SBATCH --array=1-22
+module load tacc-apptainer
+
+cd $WORK/phoronix
+./run-pts.bash \
+    --no-cpu-checks \
+    --container=apptainer \
+    --llvm=$HOME/llvm-phoronix/apptainer/main-sroa-gvn \
+    --scratch=$SCRATCH/pts-alive-runs-test \
+    -- \
+    --number-of-cores=24 \
+    --number-of-threads=112 \
+    --pts-alive2=${SLURM_ARRAY_TASK_ID}
